41b0bbcfd58c70d90aad5d9c67f56d80f8be91fc
nissyre <nissyre@gmail.com>
2017-09-11 21:52:54 +0200

    home page

--- wp-content/themes/monwol/home.php
+++ wp-content/themes/monwol/home.php
@@ -1,33 +1,60 @@
 <?php
 get_header();
 ?>
-
-
 <div class="content">
-    
-    <div class="home-images">
-        <?php
-       
-        $posts = get_posts();
-        foreach ($posts as $p) : setup_postdata($p);
+    <?php
+    $posts = get_posts();
+    $side = 'left';
+    foreach ($posts as $p) : setup_postdata($p);
+        ?>
+        <?php if ($side == 'left') { ?>
+
+            <div class="home-post"> 
+                <div class="home-post-img-left">
+                    <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($p->ID), 'single-post-thumbnail'); ?>
+                    <img class="auto-scale-img" src="<?php echo has_post_thumbnail($p->ID) ? $image[0] : $image; ?>"/>
+                </div>
+                <div class="home-post-content-right">
+                    <div class="home-post-header">
+                        <?php echo $p->post_title; ?>
+                    </div>
+                    <div class="home-post-content">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            </div>  
+
+            <?php
+            $side = 'right';
+        } else {
             ?>
             <div class="home-post"> 
-                <div class="home-post-img">
-                    
+                <div class="home-post-content-left">
+                    <div class="home-post-header">
+                        <?php echo $p->post_title; ?>
+                    </div>
+                    <div class="home-post-content">
+                        <?php the_content(); ?>
+                    </div>
                 </div>
-                <div class="home-post-header"><?php echo $p->post_title; ?></div>
-                <div class="home-post-content"><?php
-                    $subtitles = get_post_custom_values('subtitle', $p->ID);
-                    $subtitle = $subtitles[0];
-                    echo $subtitle;
-                    ?></div>
+                <div class="home-post-img-right">
+                    <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($p->ID), 'single-post-thumbnail'); ?>
+                    <img class="auto-scale-img" src="<?php echo has_post_thumbnail($p->ID) ? $image[0] : $image; ?>"/>
+                </div>
 
-            </div>
+            </div>  
+
             <?php
-        endforeach;
+            $side = 'left';
+        }
         ?>
+
     </div>
-    <div class="home-separator"></div>
+    <?php
+endforeach;
+?>
+
+
 
 </div>
 
