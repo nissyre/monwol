c6a92257f08e5b5c3cbc35525f568c88ccfc6b8a
wer0niq <wer0niq@o2.pl>
2017-09-13 12:15:56 +0200

    Kontakt

--- wp-content/themes/monwol/contact.php
+++ wp-content/themes/monwol/contact.php
@@ -2,21 +2,28 @@
 /* Template Name: Contact */
 get_header();
 ?>
-<div class="content">
-    <div class="home-post"> 
-        <div class="home-post-content-right">
-            <div class="home-post-content">
-                <?php
-                while (have_posts()) : the_post();
-                    the_content();
-                endwhile;
-                ?>
-            </div>
-                <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($p->ID), 'single-post-thumbnail'); ?>
-                <img class="auto-scale-img" src="<?php echo has_post_thumbnail($p->ID) ? $image[0] : $image; ?>"/>
+
+<div class="contact-info">
+    <div class="contact-text">
+        <div class="contact-text-header">
+            <?php
+            while (have_posts()) : the_post();
+                the_title();
+            endwhile;
+            ?>
+        </div>
+        <div class="contact-text-content">
+            <?php
+            while (have_posts()) : the_post();
+                the_content();
+            endwhile;
+            ?>
         </div>
     </div>
-</div>
+    <div class="contact-img">
+        <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($p->ID), 'single-post-thumbnail'); ?>
+        <img class="auto-scale-img" src="<?php echo has_post_thumbnail($p->ID) ? $image[0] : $image; ?>"/>
+    </div>
 </div>
 
 <?php
