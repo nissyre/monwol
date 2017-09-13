7d3280d96629d2ad7b80aaf7a427ba2a6223e992
wer0niq <wer0niq@o2.pl>
2017-09-13 13:22:27 +0200

    Uporządkowanie życiorysu.

--- wp-content/themes/monwol/biography.php
+++ wp-content/themes/monwol/biography.php
@@ -3,11 +3,18 @@
 get_header();
 ?>
 <div class="content">
-
-
-    <div class="home-post"> 
-        <div class="home-post-content-right">
-            <div class="home-post-content">
+    <div class="biography-post"> 
+        <div class="biography-post-text">
+            <div class="biography-post-header">
+                <?php
+                while (have_posts()) : the_post();
+                    the_title();
+                endwhile;
+                ?>
+            </div>
+            <div class="biography-post-content">
+                <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($p->ID), 'single-post-thumbnail'); ?>
+                <img class="biography-post-img" src="<?php echo has_post_thumbnail($p->ID) ? $image[0] : $image; ?>"/>
                 <?php
                 while (have_posts()) : the_post();
                     the_content();
@@ -16,7 +23,6 @@ get_header();
             </div>
         </div>
     </div>
-</div>
 </div>
 
 <?php
