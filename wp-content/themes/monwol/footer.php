7a3b8162e1e180ac6b35f48ed6e840d799428110
wer0niq <wer0niq@o2.pl>
2017-09-20 11:10:36 +0200

    Poprawa szerokości postów na stronie głównej.

--- wp-content/themes/monwol/footer.php
+++ wp-content/themes/monwol/footer.php
@@ -3,13 +3,13 @@
     </div>
     <div class="footer-contact">
     </div>
-</div>
 
 <img class="staff" src="http://localhost/monwol/wp-content/themes/monwol/Treble_clef_with_empty_staff.svg">
 <?php wp_footer(); ?>
 <div class="footer_content">
     <div class="footer_left">Monika Wolińska</div>
     <div class="footer_right">e-mail: info@monikawolinska.com</div>
+</div>
 </div>
 </body>
 </html>
